766366dd252038428a0609ca0ac6899a9f011b05
Valeev Azamat <vazamat@gmail.com>
2026-07-23 21:29:45 +0300

    MyThread with emit the values

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from PySide6.QtCore import Qt, QThread, Signal
 
 from ui_mainwindow import Ui_MainWindow
 
+class MyThread(QThread):
+    mysignal = Signal(int)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.running = True
+
+    def run(self):
+        for i in range(10):
+            if not self.running:
+                break
+            self.mysignal.emit(i + 1)
+            QThread.sleep(5)
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
