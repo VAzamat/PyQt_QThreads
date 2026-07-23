48c5b9abd490c6a09fd681c00872d97146d098ab
Valeev Azamat <vazamat@gmail.com>
2026-07-23 21:28:32 +0300

    MainWindow start code

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt, QThread, Signal
+
+from ui_mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
