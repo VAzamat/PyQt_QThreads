646c0797dd6d74346759d45ed57b929c567efdbb
Valeev Azamat <vazamat@gmail.com>
2026-07-23 21:31:21 +0300

    MyThread connects start/update/finish slots

--- main.py
+++ main.py
@@ -23,6 +23,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
+        self.qpbuttonThreadStart.clicked.connect(self.start_thread)
+        self.qpbuttonThreadStop.clicked.connect(self.stop_thread)
+
+    def stop_thread(self):
+        self.mythread.running = False
+
+    def start_thread(self):
+        self.qpbuttonThreadStart.setEnabled(False)
+        self.statusBar().showMessage(f"Запущен процесс сканирования...")
+
+        self.mythread = MyThread()
+        self.mythread.mysignal.connect(self.update_status, Qt.QueuedConnection)  # Подключаем сигнал обновления интерфейса
+        self.mythread.finished.connect(self.finish_thread)  # Подключаем сигнал завершения потока
+        self.mythread.start()
+
+    def update_status(self, iteration):
+        self.statusBar().showMessage(f"{iteration}. Еще сканирую...")
+
+    def finish_thread(self):
+        self.qpbuttonThreadStart.setEnabled(True)
+        self.statusBar().showMessage(f"Завершен процесс сканирования")
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
